422e18fbb713880776d693b8e7354cda20ca8d89
cauld19 <50849710+cauld19@users.noreply.github.com>
2019-11-13 19:28:35 -0800

    removed render from route

--- src/App.js
+++ src/App.js
@@ -28,10 +28,8 @@ function App() {
 					{/* Routes */}
 					<Route exact path="/" component={Products} />/>
 
-					<Route
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
-					/>
+					<Route path="/cart" component={ShoppingCart} />
+					
 				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
