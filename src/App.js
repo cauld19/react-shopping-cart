ea8da5f8a9d0bf460dfbca7b635c1d59152be45b
cauld19 <50849710+cauld19@users.noreply.github.com>
2019-11-13 20:01:56 -0800

    Stretch goal. remove item functionality

--- src/App.js
+++ src/App.js
@@ -19,17 +19,22 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = itemId => {
+		const deletedItem = cart.filter(item => item.id != itemId)
+		setCart(deletedItem)
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<CartContext.Provider value={{cart}}>
+				<CartContext.Provider value={{cart, removeItem}}>
 					<Navigation cart={cart} />
 
 					{/* Routes */}
 					<Route exact path="/" component={Products} />/>
 
 					<Route path="/cart" component={ShoppingCart} />
-					
+
 				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
-const Item = props => {
+import {CartContext} from "../contexts/CartContext"
+
+
+
+const Item = ({title, price, image, id}) => {
+
+	const { removeItem } = useContext(CartContext);
 	return (
 		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+			<img src={image} alt={`${title} book`} />
 
 
 			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<h1>{title}</h1>
+				<p>$ {price}</p>
+				<button onClick={() => removeItem(id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
